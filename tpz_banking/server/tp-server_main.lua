7f46f0434a292460fe1d52a52078a88dda3bdd44
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-21 04:10:36 +0300

    Update tp-server_main.lua

--- tpz_banking/server/tp-server_main.lua
+++ tpz_banking/server/tp-server_main.lua
@@ -46,17 +46,6 @@ end
 --[[ Functions ]]--
 -----------------------------------------------------------
 
-function Round(num, numDecimalPlaces)
-  local mult = 10^(numDecimalPlaces or 0)
-  return math.floor(num * mult + 0.5) / mult
-end
-
-function GetTableLength(T)
-  local count = 0
-  for _ in pairs(T) do count = count + 1 end
-  return count
-end
-
 function GetAccounts()
   return Accounts
 end
