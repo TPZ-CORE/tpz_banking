a6cf63aa4629bf1171df852e453e5f789f40b8b9
TPZ-CORE <152554963+TPZ-CORE@users.noreply.github.com>
2025-05-21 03:53:53 +0300

    Update fxmanifest.lua

--- tpz_banking/fxmanifest.lua
+++ tpz_banking/fxmanifest.lua
@@ -3,31 +3,15 @@ games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 author 'Nosmakos'
-description 'Titans Prudctions Banking System V 2.0 (VORP)'
-version '1.0.3'
+description 'TPZ-CORE - Banking'
+version '1.0.0'
 
 ui_page 'html/index.html'
 
-shared_scripts {
-    'config.lua',
-    'locales.lua'
-}
+shared_scripts {  'config.lua', 'locales.lua' }
+server_scripts { 'server/*.lua' }
+client_scripts { 'client/*.lua' }
 
-server_scripts {
-    'server/*.lua'
-}
-
-client_scripts {
-    'client/*.lua'
-}
-
-files {
-    'html/**/*'
-}
-
-escrow_ignore {
-    'config.lua',
-    'locales.lua',
-}
+files { 'html/**/*' }
 
 lua54 'yes'
